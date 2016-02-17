55db842329fba499b50f9ba676282646cc266572
hemagx <hemagx2@gmail.com>
2016-02-17 14:26:01 +0200

    Interface md5calc.c

--- src/login/login.c
+++ src/login/login.c
@@ -2,7 +2,7 @@
  * This file is part of Hercules.
  * http://herc.ws - http://github.com/HerculesWS/Hercules
  *
- * Copyright (C) 2012-2015  Hercules Dev Team
+ * Copyright (C) 2012-2016  Hercules Dev Team
  * Copyright (C)  Athena Dev Teams
  *
  * Hercules is free software: you can redistribute it and/or modify
@@ -234,7 +234,7 @@ bool login_check_encrypted(const char* str1, const char* str2, const char* passw
 	nullpo_ret(str2);
 	nullpo_ret(passwd);
 	safesnprintf(tmpstr, sizeof(tmpstr), "%s%s", str1, str2);
-	MD5_String(tmpstr, md5str);
+	md5->string(tmpstr, md5str);
 
 	return (0==strcmp(passwd, md5str));
 }
@@ -1470,7 +1470,7 @@ bool login_parse_client_login(int fd, struct login_session_data* sd, const char 
 		ShowStatus("Request for connection of %s (ip: %s).\n", sd->userid, ip);
 		safestrncpy(sd->passwd, password, PASSWD_LEN);
 		if (login->config->use_md5_passwds)
-			MD5_String(sd->passwd, sd->passwd);
+			md5->string(sd->passwd, sd->passwd);
 		sd->passwdenc = PWENC_NONE;
 	}
 	else
@@ -1509,7 +1509,7 @@ void login_parse_request_coding_key(int fd, struct login_session_data* sd)
 {
 	memset(sd->md5key, '\0', sizeof(sd->md5key));
 	sd->md5keylen = (uint16)(12 + rnd() % 4);
-	MD5_Salt(sd->md5keylen, sd->md5key);
+	md5->salt(sd->md5keylen, sd->md5key);
 
 	login->send_coding_key(fd, sd);
 }
@@ -1537,7 +1537,7 @@ void login_parse_request_connection(int fd, struct login_session_data* sd, const
 	safestrncpy(sd->userid, RFIFOP(fd,2), NAME_LENGTH);
 	safestrncpy(sd->passwd, RFIFOP(fd,26), NAME_LENGTH);
 	if (login->config->use_md5_passwds)
-		MD5_String(sd->passwd, sd->passwd);
+		md5->string(sd->passwd, sd->passwd);
 	sd->passwdenc = PWENC_NONE;
 	sd->version = login->config->client_version_to_connect; // hack to skip version check
 	server_ip = ntohl(RFIFOL(fd,54));
@@ -1804,14 +1804,14 @@ int login_config_read(const char *cfgName)
 			login->config->client_hash_check = config_switch(w2);
 		else if(!strcmpi(w1, "client_hash")) {
 			int group = 0;
-			char md5[33];
-			memset(md5, '\0', 33);
+			char md5hash[33];
+			memset(md5hash, '\0', 33);
 
-			if (sscanf(w2, "%d, %32s", &group, md5) == 2) {
+			if (sscanf(w2, "%d, %32s", &group, md5hash) == 2) {
 				struct client_hash_node *nnode;
 				CREATE(nnode, struct client_hash_node, 1);
 
-				if (strcmpi(md5, "disabled") == 0) {
+				if (strcmpi(md5hash, "disabled") == 0) {
 					nnode->hash[0] = '\0';
 				} else {
 					int i;
@@ -1819,7 +1819,7 @@ int login_config_read(const char *cfgName)
 						char buf[3];
 						unsigned int byte;
 
-						memcpy(buf, &md5[i], 2);
+						memcpy(buf, &md5hash[i], 2);
 						buf[2] = 0;
 
 						sscanf(buf, "%x", &byte);
